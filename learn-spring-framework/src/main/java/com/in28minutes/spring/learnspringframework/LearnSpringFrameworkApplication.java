a9ca60870aba39313d05952279a5a24f2c7501bf
Ishita Arora <iwinner21@gmail.com>
2022-10-13 19:13:55 +0530

    Enterprise

--- learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/LearnSpringFrameworkApplication.java
@@ -4,21 +4,29 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import com.in28minutes.spring.learnspringframework.enterprise.example.web.MyWebController;
 import com.in28minutes.spring.learnspringframework.game.GameRunner;
 
 @SpringBootApplication
 public class LearnSpringFrameworkApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringFrameworkApplication.class, args);
-		GameRunner runner = context.getBean(GameRunner.class);
 		
 		//MarioGame game = new MarioGame();
 		//SuperContraGame game = new SuperContraGame();
 		
 //		GameConsoleInterface game = new PacManGame();
 //		GameRunner runner = new GameRunner(game);
-//		
+		
+		
+		
+		ConfigurableApplicationContext context = SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		GameRunner runner = context.getBean(GameRunner.class);
+		
+		MyWebController controller = context.getBean(MyWebController.class);
+		
+		System.out.println(controller.returnValueFromBusinessService());
+		
 		runner.run();
 	}
 
---
+++ learn-spring-framework/src/main/java/com/in28minutes/spring/learnspringframework/enterprise/example/business/BusinessService.java
@@ -0,0 +1,21 @@
+package com.in28minutes.spring.learnspringframework.enterprise.example.business;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.in28minutes.spring.learnspringframework.enterprise.example.data.DataService;
+
+@Component
+public class BusinessService {
+	
+	@Autowired
+	private DataService dataService;
+	public long calculateSum() {
+		List<Integer> data = dataService.getData();
+		return data.parallelStream().reduce(Integer::sum).get();
+	}
+}
+
+
