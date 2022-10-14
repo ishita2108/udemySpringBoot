537a5b767c0b3c3c2ec18bf4ed83353856a52e33
Ishita Arora <iwinner21@gmail.com>
2022-10-14 16:46:53 +0530

    Logging

--- myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
+++ myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.in28minutes.springboot.myfirstwebapp.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
 	@RequestMapping("/login")
 	public String loginJsp(@RequestParam String name, ModelMap model) {
+		logger.debug("Request param is {}", name);
+		logger.info("I want this printed at info level.Request param is {}", name);
 		model.put("name", name);
 		return "login";
 		
