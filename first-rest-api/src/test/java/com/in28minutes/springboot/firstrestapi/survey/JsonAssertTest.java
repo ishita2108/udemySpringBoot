ace7a66331785c2706a3a4c4d3849b4c9e866f50
Ishita Arora <iwinner21@gmail.com>
2022-10-20 16:09:20 +0530

    Json Asserts

--- first-rest-api/src/test/java/com/in28minutes/springboot/firstrestapi/survey/JsonAssertTest.java
+++ first-rest-api/src/test/java/com/in28minutes/springboot/firstrestapi/survey/JsonAssertTest.java
@@ -9,7 +9,9 @@ class JsonAssertTest {
 	@Test
 	void jsonAssert_learningBasics() throws JSONException{
 		String expectedResponse = """
-				{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
+				{"id":"Question1",
+				"description":"Most Popular Cloud Platform Today",
+				"correctAnswer":"AWS"}
 				""";
 		
 		String actualResponse = """
@@ -19,7 +21,7 @@ class JsonAssertTest {
 				    "correctAnswer":"AWS"}
 				""";
 		
-		JSONAssert.assertEquals(expectedResponse,actualResponse,true);
+		JSONAssert.assertEquals(expectedResponse,actualResponse, false);
 	}
 
 }
--- first-rest-api/src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
+++ first-rest-api/src/test/java/com/in28minutes/springboot/firstrestapi/survey/SurveyResourceIT.java
@@ -1,8 +1,8 @@
 package com.in28minutes.springboot.firstrestapi.survey;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import org.json.JSONException;
 import org.junit.jupiter.api.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -12,17 +12,20 @@ import org.springframework.http.ResponseEntity;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class SurveyResourceIT {
 	
-	String expectedResponse ="""
-	{"id":"Question1","description":"Most Popular Cloud Platform Today","options":["AWS","Azure","Google Cloud","Oracle Cloud"],"correctAnswer":"AWS"}
-	""";
+	
 	private static String SPECIFIC_QUESTION_URL ="/surveys/Survey1/questions/Question1";
 	@Autowired
 	private TestRestTemplate template;
 
 	@Test
-	void getSurveyQuestionsById_basicScenario() {
+	void getSurveyQuestionsById_basicScenario() throws JSONException {
 		ResponseEntity<String> responseEntity = template.getForEntity(SPECIFIC_QUESTION_URL, String.class);
-		assertEquals(expectedResponse.trim(),responseEntity.getBody() );
+		String expectedResponse = """
+				{"id":"Question1",
+				"description":"Most Popular Cloud Platform Today",
+				"correctAnswer":"AWS"}
+				""";
+		JSONAssert.assertEquals(expectedResponse,responseEntity.getBody(),false);
 //		System.out.println(responseEntity.getHeaders());
 //		System.out.println(responseEntity.getBody());
 	}
